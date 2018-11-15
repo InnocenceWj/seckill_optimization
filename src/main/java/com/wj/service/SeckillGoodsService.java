e0305992d6b15a20ba7190267bcb77c134d80b85
wj <1575391348@qq.com>
2018-11-15 18:01:08 +0800

    【service】

--- src/main/java/com/wj/service/SeckillGoodsService.java
+++ src/main/java/com/wj/service/SeckillGoodsService.java
@@ -1,9 +1,17 @@
 package com.wj.service;
 
+import com.wj.entity.SeckillUser;
+import com.wj.utils.PageData;
+
+import java.awt.image.BufferedImage;
+
 /**
  * @创建人 wj
  * @创建时间 2018/11/13
  * @描述
  */
 public interface SeckillGoodsService {
+    BufferedImage createVerifyCode(String token, long goodsId);
+
+    boolean checkVerfyCode(PageData pd);
 }
